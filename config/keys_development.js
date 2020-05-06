cac240a7b1ea5113d754256c8d4924861f938fe9
Saurav Pathak <saurab.pathak.0@gmail.com>
2020-05-06 12:02:16 +0545

    final commit

---
+++ config/keys_development.js
@@ -0,0 +1,5 @@
+module.exports = {
+    MONGO_URI: 'mongodb+srv://saurav1:saurav1@cluster0-yeblu.mongodb.net/test?retryWrites=true&w=majority',
+    //MONGO_URI: 'mongodb://localhost/project',
+    SECRET: 'paniparyo'
+};
